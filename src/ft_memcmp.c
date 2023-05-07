4ba032cca6ba3937aff31fc4dd9103bf3cb27114
Nicolas Prudencio dos santos <nprudenc@c1r4p2.42sp.org.br>
2023-05-06 22:21:25 -0300

    memcmp

--- src/ft_memcmp.c
+++ src/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 21:48:05 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/05/06 22:10:34 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/05/06 22:21:08 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*str1;
-	unsigned char	*str2;
+	size_t	*str1;
+	size_t	*str2;
 
 	if (!n)
 		return (0);
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
+	str1 = (size_t *)s1;
+	str2 = (size_t *)s2;
 	while (--n && *str1 && *str1 == *str2)
 	{
 		str1++;
@@ -30,13 +30,13 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	return (*str1 - *str2);
 }
 
-int main(void)
-{
-	char	s1[] = {0, 0, 127, 0};
-	char	s2[] = {0, 0, 42, 0};
+// int main(void)
+// {
+// 	char	s1[] = {0, 0, 127, 0};
+// 	char	s2[] = {0, 0, 42, 0};
 
-	printf("str: %d\n\n", memcmp(s1, s2, 4));
-	printf("ft_str: %d\n", ft_memcmp(s1, s2, 4));
-	return (0);
-}
+// 	printf("str: %d\n\n", memcmp(s1, s2, 3));
+// 	printf("ft_str: %d\n", ft_memcmp(s1, s2, 3));
+// 	return (0);
+// }
 
