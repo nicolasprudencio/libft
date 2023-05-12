8ab0a835754bad57661c46682733556b99670278
Nicolas Prudencio dos santos <nprudenc@c1r2p4.42sp.org.br>
2023-05-12 14:35:54 -0300

    ft_strjoin.c

---
+++ src/ft_strjoin.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/12 14:04:28 by nprudenc          #+#    #+#             */
+/*   Updated: 2023/05/12 14:32:44 by nprudenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	int 	i;
+	char	*str;
+
+	i = 0;
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2) + 1;
+	str = malloc((s1_len + s2_len) * sizeof(char));
+
+	if (!str)
+		return (NULL);
+	while (*s1)
+		str[i++] = *s1++;
+	while (*s2)
+		str[i++] = *s2++;
+	str[i] = '\0';
+	return (str);
+}
