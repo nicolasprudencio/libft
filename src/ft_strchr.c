dba65f536b1a8be9edb18adcd90e3a0f990ef091
Nicolas Prudencio <nicolas-prudencio@hotmail.com>
2023-05-15 19:58:51 -0300

    ft_strchr.c

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 18:19:31 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/05/06 17:50:59 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/05/15 17:10:16 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
-{	
+{
 	if (!s)
 		return (NULL);
 	while (*s)
