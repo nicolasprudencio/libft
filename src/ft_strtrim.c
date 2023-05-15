044cca3cdd247fb778b2e8067b746d28847a600f
Nicolas Prudencio <nicolas-prudencio@hotmail.com>
2023-05-15 18:46:56 -0300

    strtrim without malloc :)

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -3,59 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nicolas <nicolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 15:11:59 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/05/12 21:13:54 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/05/15 18:46:19 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// #include <stdio.h>
-
-// size_t	ft_strlen(const char *s)
-// {
-// 	size_t	i;
-
-// 	i = 0;
-// 	while (*s)
-// 	{
-// 		i++;
-// 		s++;
-// 	}
-// 	return (i);
-// }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		end;
 	int		start;
-	char	*str_cpy;
-	char	*str_start;
-
+	char	*str;
 	end = ft_strlen(s1);
 	start = 0;
-	while (end > 0 && ft_strchr(set, s1[end - 1]))
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (NULL);
+	while (ft_strchr(set, s1[start]))
+		start++;	
+	while (ft_strchr(set, s1[end - 1]))
 		end--;
-	while (s1[start] && ft_strchr(set, s1[start]))
-		start++;
-	str_cpy = malloc((end - start + 1) * sizeof(char));
-	if (!str_cpy)
-		return (0);
-	str_start = str_cpy;
-	while (*s1)
-	{
-		if (strchr(set, *s1))
-			s1++;
-		else
-			*str_cpy++ = *s1++;
-	}
-	*str_cpy = '\0';
-	return (str_start);
+	return (ft_substr(s1, start, end - start));
 }
-
-// int main(void)
-// {
-// 	printf("%s.", ft_strtrim("###123###", "#!"));
-// 	return (0);
-// }
