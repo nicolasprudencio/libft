4d602fdacc0ff101e6ff496318e8b4d00d5bfab3
Nicolas Prudencio dos santos <nprudenc@c1r5p3.42sp.org.br>
2023-05-24 19:44:00 -0300

    strlcat

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 13:08:41 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/05/22 16:52:32 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/05/23 13:12:48 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,22 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	src_size;
 	size_t	dst_size;
 	size_t	i;
-	size_t	j;
 
+	if ((!dst || !src) && !size)
+		return (0);
 	src_size = ft_strlen(src);
 	dst_size = ft_strlen(dst);
 	i = 0;
-	j = 0;
 	if ((!src || !dst) && size == 0)
 		return (0);
 	if (dst_size >= size)
 		return (src_size + size);
 	if (size > 0)
 	{
-		while (src[j] && i < size - dst_size - 1)
+		while (src[i] && i < size - dst_size - 1)
 		{
-			dst[dst_size + i] = src[j];
+			dst[dst_size + i] = src[i];
 			i++;
-			j++;
 		}
 	}
 	dst[dst_size + i] = '\0';
