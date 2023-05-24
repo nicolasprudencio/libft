8b37aa6abdd429ff900200ec18d04ca7eee4f5e7
Nicolas Prudencio dos santos <nprudenc@c1r5p3.42sp.org.br>
2023-05-24 19:41:27 -0300

    triteri

--- src/ft_striteri.c
+++ src/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 18:31:47 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/05/17 18:37:33 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/05/23 12:59:50 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	int	i;
 
+	if (!s || !f)
+		return ;
 	i = 0;
 	while (s[i])
 	{
