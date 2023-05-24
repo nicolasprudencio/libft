cf68541b633dba87ef482f0537e57fa64f2b2d63
Nicolas Prudencio dos santos <nprudenc@c1r5p3.42sp.org.br>
2023-05-24 19:35:41 -0300

    calloc if condidition fix

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nprudenc <nprudenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 18:06:59 by nprudenc          #+#    #+#             */
-/*   Updated: 2023/05/22 15:39:12 by nprudenc         ###   ########.fr       */
+/*   Updated: 2023/05/24 19:34:30 by nprudenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 
 	i = 0;
 	size_max = nmemb * size;
-	if (size_max / size != nmemb)
+	if (size_max && size_max / size != nmemb)
 		return (NULL);
 	str = malloc(size_max);
 	if (!str)
@@ -29,34 +29,3 @@ void	*ft_calloc(size_t nmemb, size_t size)
 		str[i++] = 0;
 	return (str);
 }
-
-// int	main(void)
-// {	
-// 	int	size;
-// 	int	i;
-// 	int	*tst;
-// 	int	*tst2;
-
-// 	i = 5;
-// 	size = sizeof(int);
-// 	tst = ft_calloc(i, size);
-// 	tst2 = calloc (i, size);
-// 	tst[0] = 100003;
-// 	tst[1] = 2009903;
-// 	tst2[0] = 100003;
-// 	tst2[1] = 2009903;
-// 	printf("cal: ");
-// 	while (i--)
-// 	{
-// 		printf("%d", *tst2);
-// 	}
-// 	printf("\n\n");
-// 	printf("ft: ");
-// 	i = 5;
-// 	while (i--)
-// 	{	
-// 		printf("%d", *tst);
-// 	}
-// 	printf("\n");
-// 	return (0);
-// }
